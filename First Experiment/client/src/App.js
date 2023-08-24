4df9086c79117cbb716676c8220d1d2c5625cba1
Hariom Yadav <hariomask0111@gmail.com>
2023-08-24 12:39:07 +0530

    Signup add

--- First Experiment/client/src/App.js
+++ First Experiment/client/src/App.js
@@ -1,13 +1,15 @@
 
 import './App.css';
 import {BrowserRouter ,Route, Routes} from 'react-router-dom'
-import { SignUp } from './Componets/Private/Authantications/SignUp';
+import SignUp  from './Componets/Private/Authantications/SignUp';
+import Login from './Componets/Private/Authantications/SignIn';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<SignUp />} />
+        <Route path='/login' element={<Login /> } />
       </Routes>
     </BrowserRouter>
     
--- First Experiment/client/src/Componets/Private/Authantications/SignUp.js
+++ First Experiment/client/src/Componets/Private/Authantications/SignUp.js
@@ -1,38 +1,45 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-
-const SignUp = ()=>{
-    const [step, setStep] = useState(1);
-
-    const handleStepChange = (newStep) => {
-        setStep(newStep);
-      };
-    
-
-    return(<>
-            <div>
-      {step === 1 && <EmailOTP onNext={() => handleStepChange(2)} />}
-      {step === 2 && <MobileOTP onNext={() => handleStepChange(3)} />}
-      {step === 3 && (
-        <form>
+const SignUp = () => {
+  return (
+    <div className='signup-ct'>
+      <h1>Create Account</h1>
+      <form>
+        <div className='input'>
           <label>Name:</label>
-          <input type="text" required />
-          
-          <label>Username:</label>
-          <input type="text" required />
-          
+          <input placeholder='Name' />
+        </div>
+        <div className='input'>
+          <label>Email:</label>
+          <input placeholder='Email' />
+        </div>
+        <div className='input'>
+          <label>Email OTP:</label>
+          <input placeholder='OTP' />
+        </div>
+        <div className='input'>
+          <label>Mobile:</label>
+          <input placeholder='Mobile' />
+        </div>
+        <div className='input'>
+          <label>Mobile OTP:</label>
+          <input placeholder='OTP' />
+        </div>
+        <div className='input'>
           <label>Password:</label>
-          <input type="password" required />
-          
+          <input placeholder='Password' />
+        </div>
+        <div className='input'>
           <label>Confirm Password:</label>
-          <input type="password" required />
-          
-          <button type="submit">Sign Up</button>
-        </form>
-      )}
+          <input placeholder='Confirm PassWord' />
+        </div>
+        <Link>Google</Link>
+        <Link>github</Link>
+        <Link>facebook</Link>
+      </form>
     </div>
   );
-    </>)
-}
+};
 
-export {SignUp}
+export default SignUp;
